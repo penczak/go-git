912b9e5a37549b0aad3e474ea7a81c2fbdc7f4a8
Daniel Penczak <penczakdaniel@gmail.com>
2024-10-13 22:21:06 -0500

    add cat-file and structure for some index commands

--- go-git.go
+++ go-git.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"compress/zlib"
 	"crypto/sha1"
@@ -25,7 +26,7 @@ func main() {
 				Action: func(cCtx *cli.Context) error {
 					repoName := cCtx.Args().First()
 					safeMkdir(repoName)
-					gitFolder := filepath.Join(repoName, ".git")
+					gitFolder := filepath.Join(repoName, ".go-git")
 					safeMkdir(gitFolder)
 
 					safeWriteFile(filepath.Join(gitFolder, "HEAD"), []byte("ref: refs/heads/master\n"))
@@ -64,81 +65,132 @@ func main() {
 					write := cCtx.Bool("write")
 					// stdin := cCtx.Bool("stdin")
 
-					if filePath == "" {
-						log.Fatal("file arg required")
-					}
-
-					fileBytes, err := os.ReadFile(filePath)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					// if stdin {
-					// 	log.Fatal("TODO: support stdin")
-					// }
-
-					header := []byte("blob " + fmt.Sprint(len(fileBytes)) + "\u0000")
-
-					fullContent := append(header, fileBytes...)
-
-					fmt.Println(string(fullContent))
-
-					hasher := sha1.New()
-					hasher.Write(fullContent)
-					hash := hex.EncodeToString(hasher.Sum(nil))
-
-					fmt.Println(hash)
-
-					var b bytes.Buffer
-					w := zlib.NewWriter(&b)
-					w.Write(fullContent)
-					w.Close()
-					//fmt.Print(b.String())
-
-					//fmt.Println()
-					hashRunes := []rune(hash)
-					if write {
-						fmt.Println("writing..")
-						safeMkdir(filepath.Join(".git", "objects", string(hashRunes[:2])))
-						safeWriteFile(filepath.Join(".git", "objects", string(hashRunes[:2]), string(hashRunes[2:])), b.Bytes())
-					}
-					fmt.Println(filepath.Join(".git", "objects", string(hashRunes[:2]), string(hashRunes[2:])))
-
-					// r, err := zlib.NewReader(&b)
-					// if err != nil {
-					// 	log.Fatal(err)
-					// }
-					// io.Copy(os.Stdout, r)
-					// r.Close()
-
+					hashObject(filePath, write)
 					return nil
 				},
 			},
 			{
 				Name: "cat-file",
+				Args: true,
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:    "size",
+						Aliases: []string{"s"},
+					},
+					&cli.BoolFlag{
+						Name:    "pretty-print",
+						Aliases: []string{"p"},
+					},
+					&cli.BoolFlag{
+						Name:    "type",
+						Aliases: []string{"t"},
+					},
+					&cli.BoolFlag{
+						Name:    "all",
+						Aliases: []string{"a"},
+					},
+				},
 				Action: func(cCtx *cli.Context) error {
-					filePath := cCtx.Args().Get(0)
-					if filePath == "" {
-						log.Fatal("file arg required")
+					sha := cCtx.Args().Get(0)
+					if sha == "" {
+						log.Fatal("sha arg required")
 					}
 
-					// TODO read from .git/[0..1]/[2..40] of sha
-					// TODO take 6 character input and still find correct file
+					// read from .git/[0..1]/[2..40] of sha
+					// take 6 character SHA representation and still find correct file
+					objectsSubDir := getObjectSubDirectoryPath(sha)
+					files, err := os.ReadDir(objectsSubDir)
+					if err != nil {
+						log.Fatal("dir fatal: Not a valid object name " + sha)
+					}
+
+					filePath := ""
+					for i := 0; i < len(files); i++ {
+						if strings.HasPrefix(files[i].Name(), sha[2:]) {
+							filePath = filepath.Join(objectsSubDir, files[i].Name())
+						}
+					}
+
+					if filePath == "" {
+						log.Fatal("file fatal: Not a valid object name " + sha)
+					}
+
 					fileBytes, err := os.ReadFile(filePath)
 					if err != nil {
 						log.Fatal(err)
 					}
-					fmt.Print(string(fileBytes))
-					fmt.Println()
+
 					var b bytes.Buffer
 					b.Write(fileBytes)
 					r, err := zlib.NewReader(&b)
 					if err != nil {
 						log.Fatal(err)
 					}
-					io.Copy(os.Stdout, r)
+					buf := new(strings.Builder)
+					_, err = io.Copy(buf, r)
+					if err != nil {
+						log.Fatal(err)
+					}
 					r.Close()
+					uncompressed := buf.String()
+					// fmt.Println(uncompressed)
+					uncompressedByNullByte := strings.Split(uncompressed, "\u0000")
+					header := uncompressedByNullByte[0]
+					content := uncompressedByNullByte[1]
 
+					if cCtx.Bool("all") {
+						fmt.Print(uncompressed)
+					} else if cCtx.Bool("type") {
+						fmt.Print(strings.Split(header, " ")[0])
+					} else if cCtx.Bool("size") {
+						fmt.Print(strings.Split(header, " ")[1])
+					} else if cCtx.Bool("pretty-print") {
+						fmt.Print(content)
+					}
+
+					return nil
+				},
+			},
+			{
+				Name:  "write-tree",
+				Flags: []cli.Flag{},
+				Action: func(cCtx *cli.Context) error {
+
+					// header := []byte("tree " + fmt.Sprint(len(fileBytes)) + "\u0000")
+
+					return nil
+				},
+			},
+			{
+				Name:  "add",
+				Flags: []cli.Flag{},
+				Action: func(cCtx *cli.Context) error {
+					// pathspec := cCtx.Args().First()
+					return nil
+				},
+			},
+			{
+				Name:  "status",
+				Flags: []cli.Flag{},
+				Action: func(cCtx *cli.Context) error {
+
+					return nil
+				},
+			},
+			{
+				Name: "update-index",
+				Args: true,
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name: "add",
+					},
+					&cli.StringSliceFlag{
+						Name: "cacheinfo",
+					},
+				},
+				Action: func(cCtx *cli.Context) error {
+					cacheinfo := cCtx.StringSlice("cacheinfo")
+					fmt.Print(strings.Join(cacheinfo, "   ;   "))
 					return nil
 				},
 			},
@@ -148,6 +200,50 @@ func main() {
 	if err := app.Run(os.Args); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func hashObject(filePath string, write bool) {
+	if filePath == "" {
+		log.Fatal("file arg required")
+	}
+
+	fileBytes, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	header := []byte("blob " + fmt.Sprint(len(fileBytes)) + "\u0000")
+
+	fullContent := append(header, fileBytes...)
+
+	hasher := sha1.New()
+	hasher.Write(fullContent)
+	sha := hex.EncodeToString(hasher.Sum(nil))
+
+	fmt.Println(sha)
+
+	if write {
+		var b bytes.Buffer
+
+		w := zlib.NewWriter(&b)
+		w.Write(fullContent)
+		w.Close()
+
+		objectsSubDir := getObjectSubDirectoryPath(sha)
+		safeMkdir(objectsSubDir)
+		safeWriteFile(getObjectFilePathFromHash(sha), b.Bytes())
+	}
+}
+
+func getObjectSubDirectoryPath(hash string) string {
+	hashRunes := []rune(hash)
+	return filepath.Join(".go-git", "objects", string(hashRunes[:2]))
+}
+
+func getObjectFilePathFromHash(hash string) string {
+	dir := getObjectSubDirectoryPath(hash)
+	hashRunes := []rune(hash)
+	return filepath.Join(dir, string(hashRunes[2:]))
 }
 
 func safeWriteFile(filename string, c []byte) {
